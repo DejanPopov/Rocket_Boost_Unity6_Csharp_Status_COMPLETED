1890fdba267910525cfb68aec756b78a3ed8e151
Dejan Popov <dekipopica92@gmail.com>
2025-08-31 21:24:14 +0200

    Rocket can now fly , it has drag

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,24 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 100f;
+    Rigidbody myRigidbody;
 
     private void OnEnable()
     {
         thrust.Enable();
     }
 
-    private void Update()
+    private void Start()
     {
-        bool isThrustPressed = thrust.IsPressed();
-        Debug.Log("Thrust is " + isThrustPressed);
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() // For physics calculations, FixedUpdate is better
+    {
+        if (thrust.IsPressed())
+        {
+            myRigidbody.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }    
     }
 }
